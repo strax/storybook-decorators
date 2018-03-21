bfa8b30c41d4b467dcd65216af7c9192ee6a9802
Sami Kukkonen <sami@samikukkonen.fi>
2018-03-21 23:49:26 +0200

    Convert transparent tile to svg and setup rollup build

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import commonjs from "rollup-plugin-commonjs";
+
+import * as pkg from "./package.json";
+
+const dependencies = Object.keys({ ...pkg.dependencies, ...pkg.peerDependencies });
+
+export default {
+  input: "src/index.tsx",
+  output: {
+    format: "es",
+    file: "dist/index.js",
+    sourcemap: true
+  },
+  external: dependencies,
+  plugins: [
+    typescript(),
+    url(),
+  ]
+}
--- src/Background.ts
+++ src/Background.ts
@@ -1,20 +1,12 @@
 import styled from "styled-components";
+import background from "./transparent-tile.svg";
 
 export default styled.div`
   width: 100vw;
   height: 100vh;
   --tile-size: 30px;
   --tile-color: #fafafa;
-  background-image: linear-gradient(
-      45deg,
-      var(--tile-color) 25%,
-      transparent 25%
-    ),
-    linear-gradient(-45deg, var(--tile-color) 25%, transparent 25%),
-    linear-gradient(45deg, transparent 75%, var(--tile-color) 75%),
-    linear-gradient(-45deg, transparent 75%, var(--tile-color) 75%);
-  background-size: var(--tile-size) var(--tile-size);
-  background-position: 0 0, 0 calc(var(--tile-size) / 2),
-    calc(var(--tile-size) / 2) calc(var(--tile-size) / 2 * -1),
-    calc(var(--tile-size) / 2 * -1) 0px;
+  background-image: url(${background});
+  background-size: 30px 30px;
+  background-repeat: repeat;
 `;
