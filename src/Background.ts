12847777152cfaa55224c890e3db5cbcb0405739
Sami Kukkonen <sami@samikukkonen.fi>
2018-03-22 00:20:44 +0200

    Remove redundant CSS variables

--- src/Background.ts
+++ src/Background.ts
@@ -4,8 +4,6 @@ import background from "./transparent-tile.svg";
 export default styled.div`
   width: 100vw;
   height: 100vh;
-  --tile-size: 30px;
-  --tile-color: #fafafa;
   background-image: url(${background});
   background-size: 30px 30px;
   background-repeat: repeat;
